6ed49a3d28d3ee60e144a5beca94925208b658cd
Abdul Malik <malik.info28@gmail.com>
2024-12-12 20:48:40 +0530

    intereacting with DOM

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,37 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLTextAreaElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement
-        this.attach();
+        this.element = importedNode.firstElementChild as HTMLFormElement;   // our first <form>...</form> element
+        this.element.id = 'user-input';
 
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLTextAreaElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
+        this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 
 }
